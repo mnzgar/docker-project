da43eae15448eb48681f47e05e0ecbec3ea5b113
Miriam Núñez García <mirinu97@gmail.com>
2025-01-09 19:44:58 +0000

    :sparkles: add differents ips

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const pool = new Pool({
-  host: 'db',
+  host: process.env.DB_HOST,
   user: 'postgres',
   password: 'password',
   database: 'textsdb',
